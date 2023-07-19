62e9fdac28b4817800288224fb14fb67cb57f2d8
Ignacio Peleteiro <peleteiroignacio@gmail.com>
2023-07-19 15:59:43 -0400

    delete icono

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -5,7 +5,6 @@ import {
   AiOutlineProject,
   AiOutlineMail,
 } from 'react-icons/ai';
-import { BsPerson } from 'react-icons/bs';
 import { GrProjects } from 'react-icons/gr';
 
 const Sidenav = () => {
